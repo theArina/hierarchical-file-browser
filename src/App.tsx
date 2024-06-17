43d5ec8aa80cbcab20058bdef476335410eb1e85
Arina M <thearinamarr@gmail.com>
2024-06-17 18:22:35 +0200

    Remove unnecessary React import

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import { Component } from 'react';
 import Folder from './Folder';
 import data from '../Data.json';
 import { Node } from './interfaces';
--- src/File.tsx
+++ src/File.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import { Component } from 'react';
 import { FileProps } from './interfaces';
 
 class File extends Component<FileProps> {
